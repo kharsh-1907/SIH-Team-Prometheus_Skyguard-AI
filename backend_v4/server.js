179947fa1b922eccf0f3d5f5adbc1be8009bfc81
Harsh Khadde <khaddeharsh1907@gmail.com>
2026-09-05 19:31:31 +0530

    Prepare backend for deployment

--- backend_v4/server.js
+++ backend_v4/server.js
@@ -44,6 +44,6 @@ app.get("/", (req, res) => {
 // Server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
 });
